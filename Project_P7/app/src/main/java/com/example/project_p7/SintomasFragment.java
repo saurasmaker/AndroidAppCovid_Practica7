359691d3c043d0b0751fa7c3f75bcfebc80ea109
Iván <48927047+saurasmaker@users.noreply.github.com>
2020-11-21 19:52:35 +0100

    Esquema síntomas hecho. Falta meterle funcionalidad y contenido.

--- Project_P7/app/src/main/java/com/example/project_p7/SintomasFragment.java
+++ Project_P7/app/src/main/java/com/example/project_p7/SintomasFragment.java
@@ -1,12 +1,19 @@
 package com.example.project_p7;
 
+import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +30,17 @@ public class SintomasFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Boolean infected = false;
+
+    /*
+        Attributes PopUpDialog
+     */
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog alertDialog;
+    private TextView infoCovid;
+    private Button buttonComoActuar, buttonHospitalCercano, buttonLlamarEmergencias;
+
 
     public SintomasFragment() {
         // Required empty public constructor
@@ -60,5 +78,60 @@ public class SintomasFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sintomas, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //getView().findViewById(R.id.scrollView).setBackgroundColor(Color.BLUE);
+        getView().findViewById(R.id.buttonCheck).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                createNewInfoCheckedDialog(infected);
+            }
+        });
+
+    }
+
+
+
+    public void createNewInfoCheckedDialog(boolean infected) {
+
+        dialogBuilder = new AlertDialog.Builder(getContext());
+
+        final View infoCovidView = getLayoutInflater().inflate(R.layout.info_covid_popup, null);
+
+        //TextView textViewInfo = infoCovidView.findViewById(R.id.textViewInfoInfectated);
+        //ImageView image = infoCovidView.findViewById(R.id.imageView);
+        Button buttonComoActuar = infoCovidView.findViewById(R.id.buttonComoActuar);
+        Button buttonHospitalCercano = infoCovidView.findViewById(R.id.buttonHospitalCercano);
+        Button buttonLlamarEmergencias = infoCovidView.findViewById(R.id.buttonLlamarEmergencias);
+        Button buttonSalir = infoCovidView.findViewById(R.id.buttonSalir);
+
+        if(infected){
+            //textViewInfo.setText("Parece ser que efectivamente padeces síntomas de Covid. Aquí abajo te dejamos las cosas que puedes hacer.");
+        }
+        else{
+            //textViewInfo.setText("Nada de lo que preocuparse. La enfermedad que tienes no se corresponde con Covid 19.");
+        }
+
+        buttonComoActuar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonSalir.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        dialogBuilder.setView(infoCovidView);
+        alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        return;
     }
 }
